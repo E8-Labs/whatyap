e0ac5a674da469e0e8f63b10ffb853df2fd57088
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-11-04 23:25:29 +0500

    Message Table Charset update

--- src/models/review/chat/message.model.js
+++ src/models/review/chat/message.model.js
@@ -1,38 +1,45 @@
 const Message = (sequelize, Sequelize) => {
-  const Message = sequelize.define("Message", {
-    message: {
-      type: Sequelize.STRING, //""
-      defaultValue: "",
+  const Message = sequelize.define(
+    "Message",
+    {
+      message: {
+        type: Sequelize.STRING, //""
+        defaultValue: "",
+      },
+      media: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      voice: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      messageType: {
+        type: Sequelize.STRING,
+        defaultValue: "Text",
+      },
+      emoji: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      settlementOfferId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      chatId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
     },
-    media: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    voice: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    messageType: {
-      type: Sequelize.STRING,
-      defaultValue: "Text",
-    },
-    emoji: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    settlementOfferId: {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-    },
-    userId: {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-    },
-    chatId: {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-    },
-  });
+    {
+      charset: "utf8mb4",
+      collate: "utf8mb4_unicode_ci",
+    }
+  );
 
   // Define associations if needed
   Message.associate = function (models) {

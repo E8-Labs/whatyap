adae555eecd64d413d88a535ceedf1a6a4daa318
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-11-06 11:57:23 +0500

    yap and reviews in lite resource

--- src/resources/userprofileliteresource.js
+++ src/resources/userprofileliteresource.js
@@ -1,43 +1,47 @@
 import db from "../models/index.js";
+import { getTotalYapScore, getTotalReviews } from "../utils/user.utility.js";
 
 const Op = db.Sequelize.Op;
 
 const UserProfileLiteResource = async (user, currentUser = null) => {
-    if (!Array.isArray(user)) {
-        //////console.log("Not array")
-        return await getUserData(user, currentUser);
+  if (!Array.isArray(user)) {
+    //////console.log("Not array")
+    return await getUserData(user, currentUser);
+  } else {
+    //////console.log("Is array")
+    const data = [];
+    for (let i = 0; i < user.length; i++) {
+      const p = await getUserData(user[i], currentUser);
+      //////console.log("Adding to index " + i)
+      data.push(p);
     }
-    else {
-        //////console.log("Is array")
-        const data = []
-        for (let i = 0; i < user.length; i++) {
-            const p = await getUserData(user[i], currentUser)
-            //////console.log("Adding to index " + i)
-            data.push(p);
-        }
 
-        return data;
-    }
-}
+    return data;
+  }
+};
 
 async function getUserData(user, currentUser = null) {
+  let totalYapScore = 0;
+  let reviews = 0;
+  if (user instanceof db.User) {
+    totalYapScore = await getTotalYapScore(user);
+    reviews = await getTotalReviews(user);
+  }
+  const UserFullResource = {
+    id: user.id,
+    name: user.name,
+    profile_image: user.profile_image,
+    full_profile_image: user.full_profile_image,
+    email: user.email,
+    phone: user.phone,
+    role: user.role,
+    city: user.city,
+    state: user.state,
+    totalYapScore: totalYapScore,
+    totalReviews: reviews,
+  };
 
-    
-
-    const UserFullResource = {
-        id: user.id,
-        name: user.name,
-        profile_image: user.profile_image,
-        full_profile_image: user.full_profile_image,
-        email: user.email,
-        phone: user.phone,
-        role: user.role,
-        city: user.city,
-        state: user.state,
-    }
-
-
-    return UserFullResource;
+  return UserFullResource;
 }
 
 export default UserProfileLiteResource;
